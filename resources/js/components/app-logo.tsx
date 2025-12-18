3c1e81e9a94317910050583ae3798f8a7274e85b
Moch. Avin <mochavin16@gmail.com>
2025-12-18 22:04:56 +0700

    feat: change branding to InstaApp

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,14 +1,18 @@
+import { usePage } from '@inertiajs/react';
+import { SharedData } from '@/types';
 import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
+    const { name } = usePage<SharedData>().props;
+
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center rounded-md text-sidebar-primary-foreground">
+                <AppLogoIcon className="size-6" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
                 <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+                    {name}
                 </span>
             </div>
         </>
