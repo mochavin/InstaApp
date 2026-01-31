e87656cbca9068c64b44db326256474d28749ee1
Moch. Avin <mochavin16@gmail.com>
2026-01-31 09:01:41 +0700

    fix: update dashboard test to ensure authenticated users are redirected to posts index

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -6,8 +6,8 @@ test('guests are redirected to the login page', function () {
     $this->get(route('dashboard'))->assertRedirect(route('login'));
 });
 
-test('authenticated users can visit the dashboard', function () {
+test('authenticated users are redirected from the dashboard', function () {
     $this->actingAs($user = User::factory()->create());
 
-    $this->get(route('dashboard'))->assertOk();
+    $this->get(route('dashboard'))->assertRedirect(route('posts.index'));
 });
